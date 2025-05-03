f87ba0f91430d424006349257346d22fd35adce9
dg738 <dirgahardekaagustiantara@gmail.com>
2025-05-03 13:51:18 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <p className="bg-red-600 p-96"></p>
+    </>
+  );
+}
+
+export default App;
