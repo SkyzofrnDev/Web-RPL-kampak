c218c3b103418091b35d66ea83e0f586fcde2703
dg738 <dirgahardekaagustiantara@gmail.com>
2025-05-20 10:23:23 +0700

    Refactor CoreSkill component: update titles and descriptions for skills, enhancing clarity and relevance. Modify header text for improved context.

--- src/layout/CoreSkill/CoreSkill.jsx
+++ src/layout/CoreSkill/CoreSkill.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 
 const coreSkillsData = [
   {
-    title: "IOT",
-    description: "IoT merujuk pada jaringan objek fisik, seperti perangkat, kendaraan, dan bangunan, yang terhubung ke internet. Objek-objek ini dilengkapi dengan sensor, perangkat lunak, dan konektivitas internet, memungkinkan mereka untuk saling berkomunikasi dan bertindak secara mandiri.",
+    title: "ITS",
+    description: "ITS Atau information Technology System, Disini Kita Akan Belajar Bagaimana Cara Membuat Sebuah Program berbasis aplikasi desktop maupun ponsel, disini kita tidak hanya membuat sebuah program namun kita juga menganalisa kebutuhan pengguna hingga ke struktur database",
     svgPath: "/SvgCoreSkill/iot.svg"
   },
   {
-    title: "DEVELOPER GAME",
+    title: "PEMBUATAN GAME",
     description: "Game developer (atau pengembang gim) adalah individu atau tim yang bertanggung jawab untuk menciptakan, merancang, dan mengembangkan permainan video.",
     svgPath: "/SvgCoreSkill/Game.svg"
   },
   {
-    title: "WEB DEVELOPER",
+    title: "PEMBUATAN WEBSITE",
     description: "Web developer adalah seorang programmer yang bertugas membuat dan mengembangkan website atau world wide web. Secara garis besar, tanggung jawab web developer adalah membangun sebuah website atau aplikasi dan memastikan performa website tetap optimal.",
     svgPath: "/SvgCoreSkill/Web.svg"
   },
   {
-    title: "SCHEMA MASTERS",
-    description: "Skema atau Database Schema adalah framework atau blueprint yang digunakan oleh para programmer untuk membangun dan menulis kode database.",
+    title: "DATABASE ENGGINER",
+    description: "Database engineer adalah profesional yang perannya melibatkan penciptaan dan pemeliharaan struktur database efisien dan efektif. Hal ini bertujuan untuk menyimpan, mengatur, dan mengakses data dengan cara yang aman dan cepat. ",
     svgPath: "/SvgCoreSkill/databse2.svg"
   },
   {
-    title: "DEVELOPER APP",
+    title: "PEMBUAT APLIKASI",
     description: "App development atau pengembangan aplikasi adalah proses pembuatan aplikasi untuk perangkat mobile, desktop, atau web. Dalam era digital yang modern, aplikasi mobile telah menjadi bagian integral dari kehidupan kita, memudahkan akses informasi, hiburan, dan solusi bisnis di ujung jari pengguna.",
     svgPath: "/SvgCoreSkill/aplikasi.svg"
   },
   {
-    title: "ANALYTIC SYSTEM",
+    title: "ANALISIS SISTEM",
     description: "Analisis sistem adalah teknik pemecahan masalah yang memecah sistem menjadi bagian-bagian komponen.",
     svgPath: "/SvgCoreSkill/Database.svg"
   }
@@ -46,7 +46,7 @@ const Card = ({ title, description, svgPath }) => (
 const CoreSkill = () => {
   return (
     <div className="pt-20 lg:pt-36 px-6 bg-[#272727] text-white text-center md:p-16">
-      <p className="font-bold text-3xl mb-5 md:mb-10">Belajar Apa Saja Kita Dalam 2.5 Tahun?</p>
+      <p className="font-bold text-3xl mb-5 md:mb-10">Skill Khusus Apa Saja Kita Dalam 2.5 Tahun?</p>
       <p className="font-medium text-sm mb-10 md:text-lg md:mb-16">
         Beranjak ke tahap selanjutnya yaitu Core Skill, Skill Khusus ini akan
         dipelajari di tahun ke 2.5 masa belajar, berbeda dengan basic skills
