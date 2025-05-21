59e08871d3738dd4f85c4f2950ea6b5f00813130
dg738 <dirgahardekaagustiantara@gmail.com>
2025-05-21 07:49:15 +0700

    Integrate AOS for animations in RoadMap component; update layout and journey data structure in RoadMapClass components for improved presentation and consistency.

--- src/components/RoadMap/RoadMap.jsx
+++ src/components/RoadMap/RoadMap.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const RoadMap = ({ data }) => {
+  AOS.init({
+  duration: 6000,
+  once: true,
+  offset: 200, // Atur offset sesuai kebutuhan
+});
   return (
-    <div className="flex pt-56 flex-col items-center w-full justify-center bg-[#272727] text-white py-12 select-none">
-      <div className="text-center">
-        <p className="text-4xl font-semibold">
-          Mata Pelajaran Jurusan Selama Kelas 10
-        </p>
-        <p className="text-xl mt-2">
-          Apa saja yang kita pelajari selama 1 tahun di kelas 10?
-        </p>
-      </div>
+    <div className="flex  flex-col items-center w-full justify-center bg-[#272727] text-white py-12 select-none">
       <div className="w-full px-56 mt-20">
         {data.map((entry, index) => (
           <div
@@ -20,20 +18,32 @@ const RoadMap = ({ data }) => {
               index % 2 === 0 ? "flex-row" : "flex-row-reverse"
             }`}
           >
-            <div className="w-1/3 flex items-center justify-center px-4">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="5000"
+              className="w-1/3 flex items-center justify-center px-4"
+            >
               <img
                 className="w-full h-[280px] object-cover rounded-lg"
                 src={entry.image}
                 alt={`Journey ${index + 1}`}
               />
             </div>
-            <div className="w-1/3 flex flex-col items-center  px-4">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="3000"
+              className="w-1/3 flex flex-col items-center  px-4"
+            >
               <div className="p-3 rounded-full bg-white"></div>
               {index !== data.length - 1 && (
                 <div className="w-0.5 h-[280px] bg-white"></div>
               )}
             </div>
-            <div className="w-1/3 flex   px-4 h-[280px]">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="6000"
+              className="w-1/3 flex   px-4 h-[280px]"
+            >
               <p className="radio text-xl max-w-md">{entry.desc}</p>
             </div>
           </div>
--- src/layout/RoadMapClass/RoadMapClass12.jsx
+++ src/layout/RoadMapClass/RoadMapClass12.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import { RoadMap } from "../../components/Index";
 const journeyData = [
-    {
-      "image": "/path/to/image1.svg",
-      "desc": "Description for the first journey entry."
-    },
-    {
-      "image": "/path/to/image2.svg",
-      "desc": "Description for the second journey entry."
-    },
-    {
-      "image": "/path/to/image3.svg",
-      "desc": "Description for the third journey entry."
-    }
-  ]
+  {
+    image: "/path/to/image1.svg",
+    desc: "Description for the first journey entry.",
+  },
+  {
+    image: "/path/to/image2.svg",
+    desc: "Description for the second journey entry.",
+  },
+  {
+    image: "/path/to/image3.svg",
+    desc: "Description for the third journey entry.",
+  },
+];
 const RoadMapClass12 = () => {
   return (
-    <div>
-      <RoadMap data={journeyData}/>
+    <div className="pt-56 bg-[#272727] text-white py-12 select-none">
+      <div className="text-center">
+        <p className="text-4xl font-semibold">
+          Mata Pelajaran Jurusan Selama Kelas 11
+        </p>
+        <p className="text-xl mt-2">
+          Apa saja yang kita pelajari selama 1 tahun di kelas 11?
+        </p>
+      </div>
+      <RoadMap data={journeyData} />
     </div>
   );
 };
